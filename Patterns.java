6689b2efc2412e934b1957f212b537088d5e6255
serrano27 <serrano.soleil@student.ccm.edu>
2026-09-04 14:15:21 -0400

    Complete Week 1 Lab 2

--- Patterns.java
+++ Patterns.java
@@ -1,6 +1,31 @@
 package Labs.Week_1.scr;
 
+import java.util.Scanner;
 public class Patterns 
 {
+    public static void main(String [] args)
+    {
+        //both Pattern A and Pattern B use "+"
+    /*help from GitHub Copilot: Outer loop - controls number of rows
+    Inner Loop - controls number of symbols appearing on current row
+    "print" - used in inner loop so that symbols are on same line
+    "println"-used after inner loop to move to next row
 
+    increasing pattern - inner loop limit grows w/ outer loop count
+    decreasing pattern - inner loop limit decreases
+    */
+    
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Enter a number greater than 1 or 2: ");
+    int numberOfPluses = keyboard.nextInt(); //to not have the limits for the loops as a fixed number
+
+    //display pattern A
+    System.out.println("-------------------------------");
+    System.out.println("Pattern A");
+    System.out.println("-------------------------------");
+    //outer loop
+    for (int i = 0; i < numberOfPluses; i++)
+
+    }
 }
